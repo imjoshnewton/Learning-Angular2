a392f451f2f1262556c42130b53e0dff849bba62
newtonjd1 <josh.newton@me.com>
2017-03-24 09:36:21 -0400

    Init with final version of agular

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,13 +1,6 @@
-import { Component } from 'angular2/core';
-import {CoursesComponent} from './courses.component';
-import {AuthorsComponent} from './authors.component';
-
+import { Component } from '@angular/core';
 @Component({
-    selector: 'my-app',
-    template: `
-      <h1>Hello Angular</h1>
-      <courses></courses>
-      <authors></authors>`,
-    directives: [CoursesComponent, AuthorsComponent]
+  selector: 'my-app',
+  template: '<h1>My First Angular App</h1>'
 })
 export class AppComponent { }
